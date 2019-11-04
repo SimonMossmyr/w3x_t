7f457ee35f5bfb21bd4329d33f0d4c5edec7ad40
Simon Mossmyr <simon.mossmyr@sunstone.se>
2019-11-04 13:41:32 +0100

    Fix header flags

--- src/header.cc
+++ src/header.cc
@@ -24,19 +24,19 @@ header_type header_to_struct(string header_contents) {
     /** Map flags */
     uint32_t map_flags_raw_binary = read_int(&ss);
 
-    header.flag.hide_minimap_in_preview_screens = (bool)map_flags_raw_binary;
-    header.flag.modify_ally_priorities = (bool)map_flags_raw_binary;
-    header.flag.melee_map = map_flags_raw_binary >> 2 & 1;
-    header.flag.playable_map_size_was_large_and_has_never_been_reduced_to_medium = (bool)map_flags_raw_binary;
-    header.flag.masked_areas_are_partially_visible = (bool)map_flags_raw_binary;
-    header.flag.fixed_player_settings_for_custom_forces = (bool)map_flags_raw_binary;
-    header.flag.use_custom_forces = (bool)map_flags_raw_binary;
-    header.flag.use_custom_techtree = (bool)map_flags_raw_binary;
-    header.flag.use_custom_abilities = (bool)map_flags_raw_binary;
-    header.flag.use_custom_upgrades = (bool)map_flags_raw_binary;
-    header.flag.map_properties_menu_opened_at_least_once_since_map_creation = (bool)map_flags_raw_binary;
-    header.flag.show_water_waves_on_cliff_shores = (bool)map_flags_raw_binary;
-    header.flag.show_water_waves_on_rolling_shores = (bool)map_flags_raw_binary;
+    header.flag.hide_minimap_in_preview_screens = (bool)((map_flags_raw_binary >> 0) & 1);
+    header.flag.modify_ally_priorities = (bool)((map_flags_raw_binary >> 1) & 1);
+    header.flag.melee_map = (bool)((map_flags_raw_binary >> 2) & 1);
+    header.flag.playable_map_size_was_large_and_has_never_been_reduced_to_medium = (bool)((map_flags_raw_binary >> 3) & 1);
+    header.flag.masked_areas_are_partially_visible = (bool)((map_flags_raw_binary >> 4) & 1);
+    header.flag.fixed_player_settings_for_custom_forces = (bool)((map_flags_raw_binary >> 5) & 1);
+    header.flag.use_custom_forces = (bool)((map_flags_raw_binary >> 6) & 1);
+    header.flag.use_custom_techtree = (bool)((map_flags_raw_binary >> 7) & 1);
+    header.flag.use_custom_abilities = (bool)((map_flags_raw_binary >> 8) & 1);
+    header.flag.use_custom_upgrades = (bool)((map_flags_raw_binary >> 9) & 1);
+    header.flag.map_properties_menu_opened_at_least_once_since_map_creation = (bool)((map_flags_raw_binary >> 10) & 1);
+    header.flag.show_water_waves_on_cliff_shores = (bool)((map_flags_raw_binary >> 11) & 1);
+    header.flag.show_water_waves_on_rolling_shores = (bool)((map_flags_raw_binary >> 12) & 1);
 
     /** Max players */
     uint32_t max_players = 0;
