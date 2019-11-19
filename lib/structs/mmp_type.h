127348643daa86ff18b508abf74225f7cab3a0e9
Simon Mossmyr <simon.mossmyr@sunstone.se>
2019-11-19 01:45:47 +0100

    Remove enumeration

--- lib/structs/mmp_type.h
+++ lib/structs/mmp_type.h
@@ -1,12 +1,10 @@
 #include "utils.h"
 
-enum ICON_TYPE {gold_mine = 0, house = 1, player_start = 2};
-
 struct mmp_type {
     int format_version;
     int n_icons;
     struct icon_type {
-        enum ICON_TYPE type;
+        int type;
         int x;
         int y;
         rgba_color_type color;
